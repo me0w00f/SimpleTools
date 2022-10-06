5c64e5c397a697cf4625340af2e1b488cf6db6bf
WeepingDogel <weepingdogel@gmail.com>
2022-10-06 19:35:34 +0800

    base64 解码

---
+++ base64decode.py
@@ -0,0 +1,12 @@
+# /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+import base64
+
+parser = argparse.ArgumentParser(description="A base64 decoder script made by WeepingDogel for CTF.")
+parser.add_argument('-d', '--decode', help="decode some strings.")
+args = parser.parse_args()
+r = base64.b64decode(args.decode)
+print(r)
+
