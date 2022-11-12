46d8999f25ccd7168cfc92e3cb5d372cf52ee18e
WeepingDogel <weepingdogel@gmail.com>
2022-11-13 00:49:14 +0800

    去掉返回结果中的b'

--- base64decode.py
+++ base64decode.py
@@ -7,6 +7,6 @@ import base64
 parser = argparse.ArgumentParser(description="A base64 decoder script made by WeepingDogel for CTF.")
 parser.add_argument('-d', '--decode', help="decode some strings.")
 args = parser.parse_args()
-r = base64.b64decode(args.decode)
+r = base64.b64decode(args.decode).decode("utf-8")
 print(r)
 
